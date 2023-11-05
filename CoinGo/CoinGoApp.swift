401218ab42bdd37c126577e340f83b7979ec3f16
manishparihar948 <manish.parihar948@gmail.com>
2023-11-05 10:58:53 +0100

    Project Setup

---
+++ CoinGo/CoinGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGoApp.swift
+//  CoinGo
+//
+//  Created by Manish Parihar on 03.11.23.
+//
+
+import SwiftUI
+
+@main
+struct CoinGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinGo/Feature/Home/View/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  CoinGo
+//
+//  Created by Manish Parihar on 03.11.23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
